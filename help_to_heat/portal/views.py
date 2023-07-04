717ba03e6366a28386e44762cf82d884eb8cbb1d
Josh Powell <josh.powell@softwire.com>
2023-07-04 16:48:56 +0100

    fix import order

--- help_to_heat/portal/views.py
+++ help_to_heat/portal/views.py
@@ -2,11 +2,11 @@ import logging
 import subprocess
 
 from django.contrib.auth.decorators import login_required
+from django.db import connection
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.utils import timezone
 from django.views.decorators.http import require_http_methods
-from django.db import connection
 
 from help_to_heat import utils
 
