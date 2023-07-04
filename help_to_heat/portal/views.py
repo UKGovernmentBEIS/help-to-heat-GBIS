be39cf41c40348348a0b7112fcfa84d66255a6e1
Josh Powell <josh.powell@softwire.com>
2023-07-04 16:23:44 +0100

    Remove unused imports

--- help_to_heat/portal/views.py
+++ help_to_heat/portal/views.py
@@ -6,8 +6,6 @@ from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.utils import timezone
 from django.views.decorators.http import require_http_methods
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
 
 from help_to_heat import utils
 
