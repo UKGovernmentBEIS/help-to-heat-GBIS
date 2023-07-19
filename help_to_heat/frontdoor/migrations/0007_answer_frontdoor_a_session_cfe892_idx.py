2fcb0357e20e043074b0d75020d5b2bfa75e2f67
Sam Collard <sam.collard@softwire.com>
2023-07-19 12:40:47 +0100

    PC-None Format files

--- help_to_heat/frontdoor/migrations/0007_answer_frontdoor_a_session_cfe892_idx.py
+++ help_to_heat/frontdoor/migrations/0007_answer_frontdoor_a_session_cfe892_idx.py
@@ -4,14 +4,13 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('frontdoor', '0006_remove_answer_unique answer per page per session'),
+        ("frontdoor", "0006_remove_answer_unique answer per page per session"),
     ]
 
     operations = [
         migrations.AddIndex(
-            model_name='answer',
-            index=models.Index(fields=['session_id'], name='frontdoor_a_session_cfe892_idx'),
+            model_name="answer",
+            index=models.Index(fields=["session_id"], name="frontdoor_a_session_cfe892_idx"),
         ),
     ]
