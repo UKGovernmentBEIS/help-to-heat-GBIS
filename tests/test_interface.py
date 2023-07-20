27c68438f4e5c60b7338fb70cf5f1f199265c680
Josh Powell <josh.powell@softwire.com>
2023-07-20 16:46:27 +0100

    PC-43: update tests

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -67,9 +67,9 @@ def test_get_epc():
     }
     models.EpcRating.objects.create(**data)
 
-    found_epc = interface.api.epc.get_epc(uprn)
+    found_epc = interface.api.epc.get_epc(uprn, "England")
     assert found_epc["rating"] == data["rating"]
 
     new_uprn = "".join(random.choices(string.digits, k=5))
-    missing_epc = interface.api.epc.get_epc(new_uprn)
+    missing_epc = interface.api.epc.get_epc(new_uprn, "England")
     assert missing_epc == {}
