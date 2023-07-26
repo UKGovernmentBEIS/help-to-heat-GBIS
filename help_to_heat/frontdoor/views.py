ade77f28f55994387d378b0591a885d7e31aa3b9
Ladun Omideyi <ladun.omideyi@softwire.com>
2023-07-26 11:55:13 +0100

    PC-366: implement holding page

--- help_to_heat/frontdoor/views.py
+++ help_to_heat/frontdoor/views.py
@@ -72,7 +72,8 @@ def register_page(name):
 
 def homepage_view(request):
     session_id = uuid.uuid4()
-    next_url = reverse("frontdoor:page", kwargs=dict(session_id=session_id, page_name="country"))
+    next_url = reverse("frontdoor:holding-page")
+    # next_url = reverse("frontdoor:page", kwargs=dict(session_id=session_id, page_name="country"))
     context = {
         "next_url": next_url,
     }
