262075c5ac7fe14a67f4ada06e6b04d4158f93b3
Sydney Wu <xinni.wu@softwire.com>
2023-07-31 11:57:39 +0100

    PC-428: Add mock api

---
+++ help_to_heat/frontdoor/mock_os_api.py
@@ -0,0 +1,28 @@
+import json
+import logging
+import pathlib
+
+logger = logging.getLogger(__name__)
+
+__here__ = pathlib.Path(__file__).parent
+DATA_DIR = __here__ / "mock_os_api_data"
+
+
+class MockOSApi:
+    files = {
+        "postcode": "sample_os_api_postcode_response.json",
+        "uprn": "sample_os_api_uprn_response.json"
+    }
+
+    def __init__(self, key):
+        self.key = key
+
+    def get_by_postcode(self, postcode, offset, max_results):
+        content = (DATA_DIR / self.files["postcode"]).read_text()
+        data = json.loads(content)
+        return data
+
+    def get_by_uprn(self, uprn, dataset):
+        content = (DATA_DIR / self.files["uprn"]).read_text()
+        data = json.loads(content)
+        return data
