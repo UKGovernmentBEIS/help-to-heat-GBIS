8952da2ca0d9c24c8901c6384bb22d576bb0d03d
Josh Powell <josh.powell@softwire.com>
2023-08-07 11:16:49 +0100

    PC-NONE: fix dockerfile

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -13,6 +13,6 @@ echo "Suppliers check completed"
 
 echo "Starting app"
 
-python manage.py collectstatic
+python manage.py collectstatic --no-input
 
 watchmedo auto-restart --directory=./  --pattern=""*.py"" --recursive -- waitress-serve --port=$PORT --threads=128 --asyncore-use-poll --connection-limit=2000 --backlog=2000 help_to_heat.wsgi:application
