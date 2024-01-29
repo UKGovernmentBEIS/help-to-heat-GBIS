daabef1261eaa4adc40c636148772a01796d4662
Samuel Young <samuel.young@softwire.com>
2024-01-29 10:26:34 +0000

    PC-605: add epc check to park home flow

--- help_to_heat/frontdoor/schemas.py
+++ help_to_heat/frontdoor/schemas.py
@@ -33,6 +33,7 @@ page_order_park_home = (
     "park-home",
     "park-home-main-residence",
     "address",
+    "epc",
     "benefits",
     "household-income",
     "summary",
@@ -77,15 +78,16 @@ page_prev_next_map = {
 }
 
 page_prev_next_map_park_home = {
-    "address-select": {"prev": "address", "next": "benefits"},
-    "epc-select": {"prev": "address", "next": "benefits"},
-    "address-manual": {"prev": "address", "next": "benefits"},
+    "address-select": {"prev": "address", "next": "epc"},
+    "epc-select": {"prev": "address", "next": "epc"},
+    "address-manual": {"prev": "address", "next": "epc"},
     "northern-ireland": {"prev": "country", "next": None},
     "bulb-warning-page": {"prev": "supplier", "next": "own-property"},
     "utility-warehouse-warning-page": {"prev": "supplier", "next": "own-property"},
     "applications-closed": {"prev": "supplier", "next": None},
     "application-closed-utility-warehouse": {"prev": "supplier", "next": None},
     "park-home-application-closed": {"prev": "park-home-main-residence", "next": None},
+    "epc-ineligible": {"prev": "epc", "next": None},
 }
 
 summary_map = {
