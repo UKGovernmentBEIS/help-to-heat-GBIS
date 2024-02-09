b77d41faddc7ac96c2ced8b633d3930ff55ecd2e
Samuel Young <samuel.young@softwire.com>
2024-02-09 12:08:43 +0000

    PC-741: iniial pass at validation
    however lacks the ability to show >1 error

--- help_to_heat/portal/views.py
+++ help_to_heat/portal/views.py
@@ -1,5 +1,6 @@
 import logging
 import subprocess
+from datetime import date
 
 from django.contrib.auth.decorators import login_required
 from django.db import connection
@@ -31,34 +32,106 @@ def unauthorised_view(request):
     return render(request, "portal/unauthorised.html", {}, status=403)
 
 
-@require_http_methods(["GET"])
+@require_http_methods(["GET", "POST"])
 @login_required(login_url="portal:unauthorised")
 def homepage_view(request):
     user = request.user
-    if user.is_service_manager:
-        return service_manager_homepage_view(request)
-    elif user.is_team_leader:
-        return team_leader_homepage_view(request)
-    elif user.is_team_member:
-        return team_member_homepage_view(request)
-    else:
-        return redirect("portal:unauthorised")
+    if request.method == "GET":
+        if user.is_service_manager:
+            return service_manager_homepage_view(request)
+        elif user.is_team_leader:
+            return team_leader_homepage_view(request)
+        elif user.is_team_member:
+            return team_member_homepage_view(request)
+    elif request.method == "POST":
+        if user.is_service_manager:
+            return service_manager_homepage_view(request)
+
+    return redirect("portal:unauthorised")
 
 
-@require_http_methods(["GET"])
+def parse_date(year: str, month: str, day: str):
+    if year == "":
+        return "year-required", "year", None
+    if month == "":
+        return "month-required", "month", None
+    if day == "":
+        return "day-required", "day", None
+
+    if not year.isdecimal():
+        return "year-integer", "year", None
+    if not month.isdecimal():
+        return "month-integer", "month", None
+    if not day.isdecimal():
+        return "day-integer", "day", None
+
+    year = int(year)
+    month = int(month)
+    day = int(day)
+
+    def try_parse_date(year, month, day):
+        try:
+            return date(year, month, day)
+        except ValueError:
+            return None
+
+    check_date = try_parse_date(year, month, day)
+
+    if check_date is None:
+        return "invalid", "all", None
+
+    today = date.today()
+
+    if check_date > today:
+        return "future", "all", None
+
+    return None, check_date
+
+
+@require_http_methods(["GET", "POST"])
 @decorators.requires_service_manager
 def service_manager_homepage_view(request):
+    errors = {}
+    if request.method == "POST":
+        date_from_error, date_from_error_location, date_from = parse_date(
+            request.POST.get("from-year", None),
+            request.POST.get("from-month", None),
+            request.POST.get("from-day", None)
+        )
+        date_to_error, date_to_error_location, date_to = parse_date(
+            request.POST.get("to-year", None),
+            request.POST.get("to-month", None),
+            request.POST.get("to-day", None)
+        )
+
+        if date_from_error:
+            errors["from"] = date_from_error
+            if date_from_error_location in ["all", "year"]:
+                errors["from-year"] = True
+            if date_from_error_location in ["all", "month"]:
+                errors["from-month"] = True
+            if date_from_error_location in ["all", "day"]:
+                errors["from-day"] = True
+        if date_to_error:
+            errors["to"] = date_to_error
+            if date_to_error_location in ["all", "year"]:
+                errors["to-year"] = True
+            if date_to_error_location in ["all", "month"]:
+                errors["to-month"] = True
+            if date_to_error_location in ["all", "day"]:
+                errors["to-day"] = True
+
     template = "portal/service-manager/homepage.html"
     suppliers_to_hide = ["Bulb, now part of Octopus Energy", "ESB"]
     suppliers = [supplier for supplier in models.Supplier.objects.all() if supplier.name not in suppliers_to_hide]
     suppliers.sort(key=lambda supplier: supplier.name)
     data = {
-        "suppliers": suppliers,
+        "suppliers": suppliers
     }
     return render(
         request,
         template_name=template,
-        context={"request": request, "data": data},
+        context={"request": request, "data": data, "errors": errors, "previous": request.POST},
     )
 
 
