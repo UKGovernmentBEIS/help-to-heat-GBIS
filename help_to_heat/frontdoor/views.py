43634642b391cb8765531215cef4ff945ac78fb5
Samuel Young <samuel.young@softwire.com>
2024-03-15 12:17:58 +0000

    PC-923: register shell warning page in schemas

--- help_to_heat/frontdoor/views.py
+++ help_to_heat/frontdoor/views.py
@@ -758,12 +758,14 @@ class SupplierView(PageView):
         request_data = dict(request.POST.dict())
         request_supplier = request_data.get("supplier")
         # to be updated when we get full list of excluded suppliers
-        converted_suppliers = ["Bulb, now part of Octopus Energy", "Utility Warehouse"]
+        converted_suppliers = ["Bulb, now part of Octopus Energy", "Utility Warehouse", "Shell"]
         unavailable_suppliers = []
         if request_supplier == "Bulb, now part of Octopus Energy":
             next_page_name = "bulb-warning-page"
         if request_supplier == "Utility Warehouse":
             next_page_name = "utility-warehouse-warning-page"
+        if request_supplier == "Shell":
+            next_page_name = "shell-warning-page"
         if request_supplier in unavailable_suppliers:
             next_page_name = "applications-closed"
 
@@ -795,6 +797,11 @@ class UtilityWarehousePageView(PageView):
     def get_context(self, session_id, *args, **kwargs):
         supplier = interface.api.session.get_answer(session_id, "supplier")["supplier"]
         return {"supplier": supplier}
+
+
+@register_page("shell-warning-page")
+class UtilityWarehousePageView(PageView):
+    pass
 
 
 @register_page("applications-closed")
