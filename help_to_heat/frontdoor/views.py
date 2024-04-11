4bf5b4da6e540a02f626061b242a0e0cfcca33a5
Samuel Young <samuel.young@softwire.com>
2024-04-11 11:39:52 +0100

    PC-971: make social housing flow take priority over park home flow

--- help_to_heat/frontdoor/views.py
+++ help_to_heat/frontdoor/views.py
@@ -161,12 +161,12 @@ def get_prev_next_page_name(page_name, session_id=None):
         is_social_housing = session_data.get("own_property") == "No, I am a social housing tenant"
         receives_benefits = session_data.get("benefits") == "Yes"
 
-    if is_park_home:
-        mapping = schemas.page_prev_next_map_park_home
-        order = schemas.page_order_park_home
-    elif is_social_housing:
+    if is_social_housing:
         mapping = schemas.page_prev_next_map_social_housing
         order = schemas.page_order_social_housing
+    elif is_park_home:
+        mapping = schemas.page_prev_next_map_park_home
+        order = schemas.page_order_park_home
     else:
         mapping = schemas.page_prev_next_map
         order = schemas.page_order
