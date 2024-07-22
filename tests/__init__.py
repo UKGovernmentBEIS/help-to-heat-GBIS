ab876a80d7011944180c0d7be6f7694b90197ac7
Samuel Young <samuel.young@softwire.com>
2024-07-22 15:18:21 +0100

    PC-1121: clear db between tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,15 @@
 import os
 
 import django
+import pytest
+
+import help_to_heat
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "help_to_heat.settings")
 django.setup()
+
+@pytest.fixture(autouse=True)
+def reset_referrals():
+    help_to_heat.portal.models.Referral.objects.all().delete()
+
+    yield
