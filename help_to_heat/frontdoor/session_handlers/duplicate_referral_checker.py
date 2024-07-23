56928e3705646352830fc839ea46ea24212594a3
Samuel Young <samuel.young@softwire.com>
2024-07-23 11:30:46 +0100

    PC-1121: rename _try_find_duplicate_referral
    now accounts that it is the most recent duplicate

--- help_to_heat/frontdoor/session_handlers/duplicate_referral_checker.py
+++ help_to_heat/frontdoor/session_handlers/duplicate_referral_checker.py
@@ -6,7 +6,7 @@ class DuplicateReferralChecker:
     def __init__(self, session_id):
         self.session_id = session_id
 
-    def _try_find_duplicate_referral(self):
+    def _try_find_most_recent_duplicate_referral(self):
         session_data = api.session.get_session(self.session_id)
         uprn = session_data.get("uprn")
         if not uprn:
@@ -17,7 +17,7 @@ class DuplicateReferralChecker:
         return duplicate_referrals[0]
 
     def is_referral_a_duplicate(self):
-        referral = self._try_find_duplicate_referral()
+        referral = self._try_find_most_recent_duplicate_referral()
         return referral is not None
 
     def is_duplicate_referral_sent_to_same_energy_supplier(self):
@@ -26,14 +26,14 @@ class DuplicateReferralChecker:
 
         # needs to compare against the supplier that would've been saved
         saved_supplier = SupplierConverter(self.session_id).get_supplier_on_success_page()
-        referral = self._try_find_duplicate_referral()
+        referral = self._try_find_most_recent_duplicate_referral()
         return referral.supplier.name == saved_supplier
 
     def get_date_of_previous_referral(self):
         if not self.is_referral_a_duplicate():
             raise NoMatchingReferralInSessionException
 
-        referral = self._try_find_duplicate_referral()
+        referral = self._try_find_most_recent_duplicate_referral()
         return referral.created_at
 
 
