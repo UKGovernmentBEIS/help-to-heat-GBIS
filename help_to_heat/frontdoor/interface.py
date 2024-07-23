b3ef59e03eee73c5a94d3e0cd7a7c65af0e44ad7
Samuel Young <samuel.young@softwire.com>
2024-07-23 09:55:22 +0100

    PC-1121: account for >1 referrals made for address

--- help_to_heat/frontdoor/interface.py
+++ help_to_heat/frontdoor/interface.py
@@ -172,29 +172,34 @@ class DuplicateReferralChecker:
     def __init__(self, session_id):
         self.session_id = session_id
 
-    def is_referral_a_duplicate(self):
+    def _try_find_duplicate_referral(self):
         session_data = api.session.get_session(self.session_id)
         uprn = session_data.get("uprn")
-        return uprn and portal.models.Referral.objects.filter(data__uprn=uprn).exists()
+        if not uprn:
+            return None
+        duplicate_referrals = portal.models.Referral.objects.filter(data__uprn=uprn).order_by("-created_at")
+        if len(duplicate_referrals) == 0:
+            return None
+        return duplicate_referrals[0]
+
+    def is_referral_a_duplicate(self):
+        referral = self._try_find_duplicate_referral()
+        return referral is not None
 
     def is_duplicate_referral_sent_to_same_energy_supplier(self):
         if not self.is_referral_a_duplicate():
             raise NoMatchingReferralInSessionException
 
-        session_data = api.session.get_session(self.session_id)
-        uprn = session_data.get("uprn")
         # needs to compare against the supplier that would've been saved
         saved_supplier = SupplierConverter(self.session_id).get_supplier_on_success_page()
-        referral = portal.models.Referral.objects.get(data__uprn=uprn)
+        referral = self._try_find_duplicate_referral()
         return referral.supplier.name == saved_supplier
 
     def get_date_of_previous_referral(self):
         if not self.is_referral_a_duplicate():
             raise NoMatchingReferralInSessionException
 
-        session_data = api.session.get_session(self.session_id)
-        uprn = session_data.get("uprn")
-        referral = portal.models.Referral.objects.get(data__uprn=uprn)
+        referral = self._try_find_duplicate_referral()
         return referral.created_at
 
 
