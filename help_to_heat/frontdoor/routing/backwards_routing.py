5ecb68fd651585296a0574e9a7ab6f4fe9cde348
Samuel Young <samuel.young@softwire.com>
2024-08-22 17:35:39 +0100

    PC-1229: clarify max route length

--- help_to_heat/frontdoor/routing/backwards_routing.py
+++ help_to_heat/frontdoor/routing/backwards_routing.py
@@ -6,6 +6,8 @@ from help_to_heat.frontdoor.consts import (
 from help_to_heat.frontdoor.routing.forwards_routing import get_next_page
 
 start_page = country_page
+# in case of infinite loop ensure a route can't go on forever
+# currently the longest route is under 30 pages
 max_route_length = 100
 
 
