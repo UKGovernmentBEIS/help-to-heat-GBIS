5a264721ba9ac8d39eed58a20672e180650b55fd
Samuel Young <samuel.young@softwire.com>
2024-09-03 16:29:19 +0100

    PC-1231: don't try to catch a route error
    the page would be invalid to render in any case with an unknown back link so the error can just be propagated to our global error handler

--- help_to_heat/frontdoor/routing/backwards_routing.py
+++ help_to_heat/frontdoor/routing/backwards_routing.py
@@ -3,10 +3,7 @@ from help_to_heat.frontdoor.consts import (
     govuk_start_page,
     unknown_page,
 )
-from help_to_heat.frontdoor.routing import (
-    CouldNotCalculateRouteException,
-    calculate_route,
-)
+from help_to_heat.frontdoor.routing import calculate_route
 
 start_page = country_page
 # in case of infinite loop ensure a route can't go on forever
@@ -19,11 +16,8 @@ def get_prev_page(current_page, answers):
     if current_page == govuk_start_page:
         return unknown_page
 
-    try:
-        route = calculate_route(answers, current_page)
+    route = calculate_route(answers, current_page)
 
-        # if the page has been found, the last item in the list (index -1) will be the current page
-        # so, the item before the last one (index -2) will be the previous page
-        return route[-2]
-    except CouldNotCalculateRouteException:
-        return unknown_page
+    # if the page has been found, the last item in the list (index -1) will be the current page
+    # so, the item before the last one (index -2) will be the previous page
+    return route[-2]
