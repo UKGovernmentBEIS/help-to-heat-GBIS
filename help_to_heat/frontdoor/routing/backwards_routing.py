526db12c9e7aaa7e9e774ed32b60bd3abdcb5b54
Samuel Young <samuel.young@softwire.com>
2024-09-04 14:05:00 +0100

    PC-1228: rename route to journey

--- help_to_heat/frontdoor/routing/backwards_routing.py
+++ help_to_heat/frontdoor/routing/backwards_routing.py
@@ -8,26 +8,26 @@ from help_to_heat.frontdoor.consts import (
 from help_to_heat.frontdoor.routing.forwards_routing import get_next_page
 
 start_page = country_page
-# in case of infinite loop ensure a route can't go on forever
-# currently the longest route is under 30 pages
-max_route_length = 100
+# in case of infinite loop ensure a journey can't go on forever
+# currently the longest journey is under 30 pages
+max_journey_length = 100
 
 
 def get_prev_page(current_page, answers):
-    route_page = start_page
-    route = deque([govuk_start_page])
+    journey_page = start_page
+    journey = deque([govuk_start_page])
 
-    while len(route) < max_route_length:
-        if route_page == current_page:
-            return route.pop()
+    while len(journey) < max_journey_length:
+        if journey_page == current_page:
+            return journey.pop()
 
-        if route_page == unknown_page:
+        if journey_page == unknown_page:
             return unknown_page
 
-        next_page = get_next_page(route_page, answers)
+        next_page = get_next_page(journey_page, answers)
 
-        route.append(route_page)
+        journey.append(journey_page)
 
-        route_page = next_page
+        journey_page = next_page
 
     return unknown_page
