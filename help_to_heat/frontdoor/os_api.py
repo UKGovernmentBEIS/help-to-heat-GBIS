19a32b327c069dfbbf03096a9574e2397eaf4c6e
Samuel Young <samuel.young@softwire.com>
2025-03-26 13:56:38 +0000

    PC-1770: Add postcode to OS API logs
    this will help us identify any patterns in postcodes that are causing us ratelimit issues

--- help_to_heat/frontdoor/os_api.py
+++ help_to_heat/frontdoor/os_api.py
@@ -28,16 +28,23 @@ class OSApi:
                 return self.perform_request(url)
 
             except requests.exceptions.HTTPError or requests.exceptions.RequestException as e:
+                # we log the postcode here in case of error to help diagnose if there are patterns in the kinds of
+                # postcodes that cause a ratelimit hit
+                # surrounding logs will only contain the session ID and general flow throughout the site
+                # so, it is not possible to attribute the postcode to any other details
+                # the logs themselves are retained for a week in CloudWatch
                 status_code = e.response.status_code
                 if status_code == HTTPStatus.TOO_MANY_REQUESTS:
-                    logger.error(f"The OS API usage limit has been hit for API key at index {index}.")
+                    logger.error(
+                        f"The OS API usage limit has been hit for API key at index {index} for postcode {postcode}."
+                    )
                     if index == len(self.keys) - 1:
-                        logger.error("The OS API usage limit has been hit for all API keys")
+                        logger.error(f"The OS API usage limit has been hit for all API keys for postcode {postcode}.")
                         raise ThrottledApiException
                     else:
                         continue
 
-                logger.exception("An error occurred while attempting to fetch addresses.")
+                logger.exception(f"An error occurred while attempting to fetch addresses for postcode {postcode}.")
                 break
         return []
 
