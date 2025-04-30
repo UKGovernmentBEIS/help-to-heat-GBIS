a623a5ae2482859ec1563d515233cfb5feed0b9c
XakSenSoftwire <Xak.Sen@softwire.com>
2025-04-30 12:00:29 +0100

    PC-1850: Remove extra files

--- static_error_pages/main.js
+++
@@ -1,7 +0,0 @@
-document.body.className = document.body.className
-  ? document.body.className + " js-enabled"
-  : "js-enabled";
-
-if (window.GOVUKFrontend) {
-  window.GOVUKFrontend.initAll();
-}
